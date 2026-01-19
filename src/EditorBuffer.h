dc71c9b8b65bd4a5b5a1acbe6ff3260b3b450945
TeraMaster1 <teraboykisser@proton.me>
2026-01-19 19:02:40 +0100

    say gex

---
+++ src/EditorBuffer.h
@@ -0,0 +1,28 @@
+
+#pragma once
+#ifndef TEXTREDIGERARE_CPP_EDITORBUFFER_H
+#define TEXTREDIGERARE_CPP_EDITORBUFFER_H
+#include <fstream>
+#include <string>
+#include <vector>
+
+
+class EditorBuffer {
+public:
+  explicit EditorBuffer(const std::string& path);
+  void print();
+  void printAll();
+  void editLine(std::string line);
+  void saveWritten();
+  void selectLine(int line);
+  int getCurrentLine();
+  ~EditorBuffer();
+private:
+  int currentLine;
+  std::string path;
+  std::vector<std::string> text;
+  std::ifstream infile;
+};
+
+
+#endif //TEXTREDIGERARE_CPP_EDITORBUFFER_H
---
+++ src/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+#include "EditorBuffer.h"
+
+int main() {
+  std::cout << "Programmet startade" << std::endl;
+  std::string path;
+  std::cout << "Skriv in filen: ";
+  std::cin >> path;
+  EditorBuffer buffer = EditorBuffer(path);
+  buffer.print();
+  char opt = 'l';
+  while (opt != 'a')
+  {
+    std::cout<< "Vill du [a]vsluta, [v]älja rad, [l]äsa hela fileninnehållet, [s]kriva till en rad, s[p]ara innehållet? ";
+    std::cin >> opt;
+    switch (opt){
+
+    case 'l': {
+      buffer.print();
+      break;
+    }
+      case 'v': {
+      std::cout<< "Välj rad: ";
+      int line;
+      std::cin>> line;
+      buffer.selectLine(line);
+      break;
+    }
+    case 's':
+      {
+        buffer.print();
+        std::string strLine;
+        std::cout << "Skriv vad du vill ska stå på raden: ";
+        std::cin.ignore();
+        std::getline(std::cin, strLine);
+        buffer.editLine(strLine);
+        break;
+      }
+    case 'p':
+      {
+        buffer.saveWritten();
+        break;
+      }
+    default:
+      {
+        std::cout << "Ogilitgt alternativ, försök igen." << std::endl;
+        break;
+      }
+    }
+
+  }
+  return 0;
+}
